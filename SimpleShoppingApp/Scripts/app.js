fb881fbfe6d1ec44f554a2c8cf0611355a9f4d8a
Kristen <kristenpetersen04@gmail.com>
2019-04-08 15:44:31 +0200

    First Commit

---
+++ SimpleShoppingApp/Scripts/app.js
@@ -0,0 +1,50 @@
+﻿var currentList = {};
+
+function createShoppingList()
+{
+    currentList.name = $("#shoppingListName").val();
+    currentList.name = new Array();
+    
+    showShoppingList();
+
+}
+
+function showShoppingList()
+{
+
+$("#shoppingListTitle").html(currentList.name);
+$("#shoppingListItems").empty();
+
+$("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+
+    $("#newItemName").focus();
+    $("#newItemName").keyup(function (event) {
+        if (event.keyCode == 13)
+        {
+
+            addItem();
+        }
+
+    });
+}
+function addItem()
+{
+
+}
+$(document).ready(function () {
+    console.info("im ready!");
+    $("#shoppingListName").focus();
+    $("#shoppingListName").keyup(function (event)
+    {
+        if (event.keyCode == 13)
+        {
+            createShoppingList();
+        }
+    }
+
+
+
+    });
+});
+
